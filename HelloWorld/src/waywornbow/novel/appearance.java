28d5ab83e7e5b02f653909557bc2d0ded451b6b5
jandros <jeff@jeffandros.com>
2015-02-25 21:37:48 -0700

    create class for character appearance

---
+++ HelloWorld/src/waywornbow/novel/appearance.java
@@ -0,0 +1,16 @@
+package waywornbow.novel;
+
+//todo: put imports here
+
+class appearance{
+
+  character character;
+  String emotion
+
+  public appearance(character character, String emotion){
+    this.emotion = emotion;
+    this.character = character;
+  }
+  
+  //todo: create getters for both character and emotion
+}
