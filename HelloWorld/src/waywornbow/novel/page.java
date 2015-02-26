d95c1361872a81f006182996b2f74b9f83f271cd
jandros <jeff@jeffandros.com>
2015-02-25 21:57:59 -0700

    adding page class

---
+++ HelloWorld/src/waywornbow/novel/page.java
@@ -0,0 +1,15 @@
+package waywornbow.novel;
+
+//todo: put in appropriate imports here
+
+class page{
+  String background;
+  String dialog;
+  appearance[] appearances
+  
+  public page(String background, String dialog){
+    //todo: set up variables like the other classes
+  }
+  
+  //todo: create setters and getters
+}
