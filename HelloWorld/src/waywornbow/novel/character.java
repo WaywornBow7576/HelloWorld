36e75d9e0b94826fb4b653b5754c012b76040fad
jandros <jeff@jeffandros.com>
2015-02-25 21:23:10 -0700

    new class to represent character

---
+++ HelloWorld/src/waywornbow/novel/character.java
@@ -0,0 +1,12 @@
+package waywornbow.novel;
+
+//todo: put appropriate imports here
+
+class character{
+  String name;
+  String[] images;
+  
+  public character(String name){
+    this.name = name;
+  }
+}
